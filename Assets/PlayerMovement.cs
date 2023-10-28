14e36c9e9d5365d1818150a6b6d16d513efb8270
TauVip <Abylai1393@gmail.com>
2023-10-28 10:57:07 +0600

    first commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigitbody rb;
+
+    public float runSpeed = 500f;
+    public float strafeSpeed = 500f;
+    public float jumpForce = 15f;
+
+    protected bool strafeLeft = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
