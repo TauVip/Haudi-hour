659c179d15b7daa513713abe2c51a16eddb1f603
TauVip <Abylai1393@gmail.com>
2023-10-28 12:42:17 +0600

    commit 2

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,17 +2,66 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigitbody rb;
+    public Rigidbody rb;
 
     public float runSpeed = 500f;
     public float strafeSpeed = 500f;
     public float jumpForce = 15f;
 
     protected bool strafeLeft = false;
+    protected bool strafeRight = false;
+    protected bool doJump = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("a"))
+        {
+            strafeLeft = true;
+        } else
+        {
+            strafeLeft = false;
+        }
+
+        if(Input.GetKey("d")) 
+        {
+            strafeRight = true;
+        }
+        else
+        {
+            strafeRight = false;
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            doJump = true;
+        }
+
+        if(transform.position.y < -5f)
+        {
+            Debug.Log("Конец игры!");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, runSpeed* Time.deltaTime);
+
+        if(strafeLeft)
+        {
+            rb.AddForce(-strafeSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (strafeRight)
+        {
+            rb.AddForce(strafeSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (doJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            doJump = false;
+        }
     }
 }
